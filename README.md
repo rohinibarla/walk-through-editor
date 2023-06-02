5446153b2d1e7f06bdb802ad778d39584a47e7a8
Rohini Kumar Barla <rohinibarla@enhance42.com>
2023-06-02 23:12:06 +0530

    single file and modular version to open walkthough from the code and trace given

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # walk-through-editor
 A simple NodeJS application to Write Code, Walkthrough, variables
+
+# Write in Single File
+[Write - single file](writer)
+
+# Writer modular version
+[Writer - modular](writer/modular)
+
+# TODO
+In Modular the generated code the code font in walkthrough is smaller, when compared to the single index.html file.
---
+++ writer/modular/script.js
@@ -0,0 +1,95 @@
+var editorData = {};
+
+function setupAceEditors() {
+    editorData.code = ace.edit("editor_code");
+    editorData.code.setTheme("ace/theme/monokai");
+    editorData.code.session.setMode("ace/mode/c_cpp");
+
+    editorData.json = ace.edit("editor_json");
+    editorData.json.setTheme("ace/theme/monokai");
+    editorData.json.session.setMode("ace/mode/json");
+}
+
+// Other functions
+function traceVaraibles(jsonText) {
+    var jsonData = JSON.parse(jsonText);
+    // First, map to get all variables
+    var variables = jsonData.map(function(obj) {
+        return obj.variable;
+    });
+
+    // Then, filter out unique values
+    var uniqueVariables = [...new Set(variables)].filter(Boolean);
+    console.log(uniqueVariables);  // Outputs: ["i", "output"]
+
+    return uniqueVariables.map(va => `<th>${va}</th>`).join('');
+}
+
+// Create HTML
+function createHtml() {
+    var code = editorData.code.getValue();
+    var walkthrough = editorData.json.getValue();
+    var trace_variables = traceVaraibles(walkthrough);
+    var htmlContent = createHtmlContent(code, walkthrough, trace_variables);
+
+    openInNewTab(htmlContent);
+}
+
+// Create HTML Content
+function createHtmlContent(code, walkthrough, trace_variables) {
+    var work_around = `</scri` + `pt>`;
+    
+    var htmlHead = createHtmlHead(work_around);
+    var htmlBody = createHtmlBody(code, walkthrough, trace_variables, work_around);
+    
+    return `<?xml version="1.0" encoding="UTF-8"?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    ${htmlHead}
+    ${htmlBody}
+</html>`;
+}
+
+// Create HTML Head
+function createHtmlHead(work_around) {
+    return `<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+<title>Walk-through C loops 01</title> 
+<script type="text/javascript" src="https://e42.dev/c/assets/scripts/e42_all_min.js">${work_around}
+<link type="text/css" rel="stylesheet" href="https://e42.dev/c/assets/css/e42_all_min.css"></link>
+<style type="text/css">...</style>
+</head>`;
+}
+
+// Create HTML Body
+function createHtmlBody(code, walkthrough, trace_variables, work_around) {
+    return `<body>
+<ol class="interactivities" id="interactivities">
+    <li title="c-loops-walkthrough-01" id="c-loops-walkthrough-01">
+        <div class="hc-included">
+            <div>
+                <p>Walk-through the sample code.</p>
+                <div class="e42_walkthrough">
+                    <pre>${code}</pre>
+                    <table style="font-size:x-large"><tr>${trace_variables}</tr></table>
+                    <script type="text/javascript">//<![CDATA[
+                        e42_walkthrough.setup.push(${walkthrough})
+                    //]]> ${work_around}
+                </div>
+            </div>
+        </div>
+    </li>
+</ol>
+<div class="vstreport"></div>
+</body>`;
+}
+
+// Open in New Tab
+function openInNewTab(htmlContent) {
+    var blob = new Blob([htmlContent], { type: "text/html;charset=utf-8" });
+    var url = URL.createObjectURL(blob);
+    
+    // Open the URL in a new tab
+    window.open(url, '_blank');
+}
+
+window.onload = setupAceEditors;
